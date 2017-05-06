51a51eed3ef9db9de38fa6d7de08df2a6fef57ed
judith <jmplibrary@gmail.com>
2017-05-06 16:28:03 +0100

    settings updated with host specified

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kxo*(((#cjd_m^*-5#wk__^p5d!c(x1)lp(2+oz4nl%+1kmgwy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
